e4712a2999b6e31d3c369f9a67f6eaeb1ca29246
vishaaal <officialvk02@gmail.com>
2023-09-13 22:39:33 +0530

    delete users routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 //UPDATE
@@ -10,6 +11,7 @@ router.put("/:id", async (req, res) => {
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
+      await Post.deleteMany({ username: user.username });
       const updatedUser = await User.findByIdAndUpdate(
         req.params.id,
         {
@@ -25,5 +27,25 @@ router.put("/:id", async (req, res) => {
     res.status(401).json("You can update only your account");
   }
 });
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        try {
+          await Post.deleteMany({ username: user.username });
+          await User.findByIdAndDelete(req.params.id);
+          res.status(200).json("User has been deleted...");
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } catch (err) {
+        res.status(404).json("User not found!");
+      }
+    } else {
+      res.status(401).json("You can delete only your account!");
+    }
+  });
 
 module.exports = router;
