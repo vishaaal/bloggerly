3dbcd5c04b52e4c5d7a6076e5ef8ef7c79053d33
vishaaal <officialvk02@gmail.com>
2023-09-13 16:40:18 +0530

    [feat] Post.jsx

--- client/src/post/Post.jsx
+++ client/src/post/Post.jsx
@@ -1,7 +1,29 @@
-import "./post.css"
+import "./post.css";
 
 export default function Post() {
   return (
-    <div className="post">Post</div>
-  )
+    <div className="post">
+      <img
+        className="postImg"
+        src="https://images.pexels.com/photos/3944091/pexels-photo-3944091.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt=""
+      />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
+        </div>
+        <span className="postTitle">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam esse
+        magni incidunt, magnam amet perferendis quasi totam laboriosam a nihil
+        natus autem quam.
+      </p>
+    </div>
+  );
 }
