aa30659e4fa851b685aa9408906f5d04c079b9ed
vishaaal <officialvk02@gmail.com>
2023-09-13 16:05:57 +0530

    [feat] Header

--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -2,6 +2,16 @@ import "./header.css"
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSm">
+                React & Node
+            </span>
+            <span className="headerTitleLg">
+                Blog
+            </span>
+            <img src="https://images.pexels.com/photos/4793154/pexels-photo-4793154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="headerImg"/>
+        </div>
+    </div>
   )
 }
