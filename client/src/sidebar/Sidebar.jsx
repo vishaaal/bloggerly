d1fa7151664983ebadc6e7b169cbfc86ee3185c8
vishaaal <officialvk02@gmail.com>
2023-09-13 16:23:47 +0530

    [feat] Sidebar

--- client/src/sidebar/Sidebar.jsx
+++ client/src/sidebar/Sidebar.jsx
@@ -1,7 +1,40 @@
-import "./sidebar.css"
+import "./sidebar.css";
 
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/17959888/pexels-photo-17959888/free-photo-of-glacier-iceland-snow-wood.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          cumque dolor eum sequi, veritatis ab. Libero voluptate itaque ullam,
+          quos ratione enim culpa.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sports</li>
+          <li className="sidebarListItem">Cinema</li>
+          <li className="sidebarListItem">Tech</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+          <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
