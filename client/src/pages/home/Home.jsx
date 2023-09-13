709b8d113b5f42d8cb79402c38e99e32fb8bf5f1
vishaaal <officialvk02@gmail.com>
2023-09-13 16:23:34 +0530

    import components Home and add flex

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import Header from "../../header/Header";
 import "./home.css";
+import {Sidebar} from "../../sidebar/Sidebar"; 
+import {Posts} from "../../posts/Posts"
 
 export const Home = () => {
   return (
-    <div>
+    <>
       <Header />
-      Homepage
-    </div>
+      <div className="home">
+        <Posts/>
+        <Sidebar/>
+       </div>  
+
+    </>
   );
 };
