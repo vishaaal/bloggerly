80cf0d752c399a84b950c655296b221ee8b3123a
vishaaal <officialvk02@gmail.com>
2023-09-14 00:02:51 +0530

    update post routes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -14,6 +14,30 @@ router.post("/", async (req, res) => {
     }
 });
 
+//UPDATE POST
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.username === req.body.username) {
+        try {
+          const updatedPost = await Post.findByIdAndUpdate(
+            req.params.id,
+            {
+              $set: req.body,
+            },
+            { new: true }
+          );
+          res.status(200).json(updatedPost);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(401).json("You can update only your post!");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 module.exports = router;
