fbc5365198ef226548bba1ffdf613d1980f7e43f
vishaaal <officialvk02@gmail.com>
2023-09-13 19:20:55 +0530

    [feat] add Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import { Home } from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Write from "./pages/write/Write"
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single"
 
 function App() {
+  const user = true;
   return (
-    <div className="App">
-     <TopBar/>
-     <Home/>
-    </div>
-  );
-}
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route path="/" exact element={<Home />}>
+        </Route>
+        <Route path="/register" element={user?<Home/>:<Register />}>
+        </Route>
+        <Route path="/login" element={user?<Home/>:<Login/>}>
+        </Route>
+        <Route path="/write" element={user?<Write/>:<Register/>}>
+        </Route>
+        <Route path="/settings" element={user?<Settings/>:<Register/>}>
+        </Route>
+        <Route path="/post/:postId" element={<Single/>}>
+        </Route>
+      </Routes>
+
+    </Router>
+  );}
 
 export default App;
