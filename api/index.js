f0a840b837d719f91d3c24574a6ee3c3da4215ca
vishaaal <officialvk02@gmail.com>
2023-09-13 21:58:54 +0530

    configure index.js for auth

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
-dotenv.config()
-mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")).catch(err=>console.log(err));
+dotenv.config();
+app.use(express.json());
 
-app.use("/",(req,res)=>{
-    console.log("hey this is main url");
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-app.listen("5000", ()=>{
-    console.log("Backend is running")
-})
+app.use("/api/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running");
+});
