663339f8b717d189d59d201af1b8a632d45596da
vishaaal <officialvk02@gmail.com>
2023-09-14 12:07:00 +0530

    make Posts dynamic

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,12 +1,12 @@
 import Post from "../post/Post";
 import "./posts.css";
 
-export const Posts = () => {
+export const Posts = ({posts}) => {
   return (
     <div className="posts">
-      <Post />
-      <Post />
-      <Post />
+      {posts.map(p=>(
+        <Post post={p}/>
+      ))}
     </div>
   );
-};
+}; 
