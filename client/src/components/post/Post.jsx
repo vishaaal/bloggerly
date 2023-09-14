1df313a907c1ddd26d0669127a6b8e442a478ece
vishaaal <officialvk02@gmail.com>
2023-09-14 12:07:12 +0530

    make post dynamic

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,29 +1,26 @@
+import { Link } from "react-router-dom";
 import "./post.css";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://images.pexels.com/photos/3944091/pexels-photo-3944091.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt=""
-      />
+      {post.photo && <img className="postImg" src={post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        </span>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam esse
-        magni incidunt, magnam amet perferendis quasi totam laboriosam a nihil
-        natus autem quam.
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 }
