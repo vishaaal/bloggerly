dba2e2843795998007b391ea02f4d0bfc15e0d57
vishaaal <officialvk02@gmail.com>
2023-09-14 12:25:24 +0530

    filter search by user

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,20 +3,22 @@ import "./home.css";
 import {Sidebar} from "../../components/sidebar/Sidebar"; 
 import {Posts} from "../../components/posts/Posts"
 import axios from "axios";
-// import { useLocation } from "react-router";
+import { useLocation } from "react-router";
 import { useEffect, useState } from "react";
 
 export const Home = () => {
+
   const [posts, setPosts] = useState([]);
-  // const { search } = useLocation();
+  const {search} = useLocation();
+
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts");
+      const res = await axios.get("/posts"+search);
       setPosts(res.data)
     };
     fetchPosts();
-  }, []);
+  }, [search]);
   return (
     <>
       <Header />
