9003f7ea87b2310bc3e7b6fd685cba8e32813a1c
vishaaal <officialvk02@gmail.com>
2023-09-14 12:15:38 +0530

    make singlepost dynamic

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,42 +1,45 @@
+import { useLocation } from "react-router-dom";
 import "./singlepost.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function SinglePost() {
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+    };
+    getPost();
+  }, [path]);
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img
-          src="https://images.pexels.com/photos/3601425/pexels-photo-3601425.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-          className="singlePostImg"
-        />
+        {post.photo && (
+          <img
+            src={post.photo}
+            alt=""
+            className="singlePostImg"
+          />
+        )}
         <h1 className="singlePostTitle">
-          Lorem ipsum dolor sit.
+          {post.title}
           <div className="singlePostEdit">
-            <i class="singlePostIcon fa-regular fa-pen-to-square"></i>
-            <i class="singlePostIcon fa-solid fa-trash-can"></i>
+            <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
+            <i className="singlePostIcon fa-solid fa-trash-can"></i>
           </div>
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
-            Author: <b>Vishal</b>
+            Author: <b>{post.username}</b>
           </span>
-          <span className="singlePostDate">1 hour ago</span>
+          <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
         <p className="singlePostDesc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illum
-          rem natus sapiente magni iste dolor tenetur dignissimos libero
-          accusamus quasi quos eaque, ex sunt tempore quam et, consequatur
-          perspiciatis, voluptatem praesentium. Aliquid explicabo nihil
-          distinctio quod beatae recusandae provident. Inventore consequuntur
-          harum ullam, dolore, mollitia nam nihil exercitationem molestias, sed
-          veritatis iusto odio saepe rerum? Molestiae voluptatem eos accusantium
-          architecto ipsa, esse ipsum iure suscipit rem? Possimus consequatur
-          cumque accusamus beatae, ex quasi nemo blanditiis quo esse neque earum
-          fugit provident, vitae modi minus assumenda quidem perferendis ipsa
-          reprehenderit veniam quisquam placeat. Natus enim officia, ratione ad
-          expedita quis. Quaerat dolorem magnam error ipsam doloribus architecto
-          saepe sed. Praesentium deleniti quaerat a eaque. Expedita cumque cum
-          autem voluptas similique.
+          {post.desc}
         </p>
       </div>
     </div>
