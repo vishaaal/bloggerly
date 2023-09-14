b09210d0a2602f89b21626d2d710a899630e3cdd
vishaaal <officialvk02@gmail.com>
2023-09-14 12:07:26 +0530

    fetch data axios Home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,13 +2,26 @@ import Header from "../../components/header/Header";
 import "./home.css";
 import {Sidebar} from "../../components/sidebar/Sidebar"; 
 import {Posts} from "../../components/posts/Posts"
+import axios from "axios";
+// import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  // const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setPosts(res.data)
+    };
+    fetchPosts();
+  }, []);
   return (
     <>
       <Header />
       <div className="home">
-        <Posts/>
+        <Posts posts={posts}/>
         <Sidebar/>
        </div>  
 
