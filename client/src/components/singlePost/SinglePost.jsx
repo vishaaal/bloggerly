88c07a2557f31f43a68a582e04fb5b368079e4db
vishaaal <officialvk02@gmail.com>
2023-09-14 12:27:39 +0530

    Link author to filter user

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./singlepost.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -34,7 +34,10 @@ export default function SinglePost() {
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
-            Author: <b>{post.username}</b>
+            Author: 
+            <Link to={`/?user=${post.username}`} className="link">
+            <b>{post.username}</b>
+            </Link>
           </span>
           <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
