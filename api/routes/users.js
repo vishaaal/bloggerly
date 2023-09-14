59eebe6363b201b8b6c3f9319f0ae6a8dceaa9e7
vishaaal <officialvk02@gmail.com>
2023-09-14 19:54:04 +0530

    reformat code

--- api/routes/users.js
+++ api/routes/users.js
@@ -11,7 +11,6 @@ router.put("/:id", async (req, res) => {
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
-      await Post.deleteMany({ username: user.username });
       const updatedUser = await User.findByIdAndUpdate(
         req.params.id,
         {
@@ -24,39 +23,39 @@ router.put("/:id", async (req, res) => {
       res.status(500).json(err);
     }
   } else {
-    res.status(401).json("You can update only your account");
+    res.status(401).json("You can update only your account!");
   }
 });
 
 //DELETE
 router.delete("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id) {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
       try {
-        const user = await User.findById(req.params.id);
-        try {
-          await Post.deleteMany({ username: user.username });
-          await User.findByIdAndDelete(req.params.id);
-          res.status(200).json("User has been deleted...");
-        } catch (err) {
-          res.status(500).json(err);
-        }
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
       } catch (err) {
-        res.status(404).json("User not found!");
+        res.status(500).json(err);
       }
-    } else {
-      res.status(401).json("You can delete only your account!");
+    } catch (err) {
+      res.status(404).json("User not found!");
     }
-  });
+  } else {
+    res.status(401).json("You can delete only your account!");
+  }
+});
 
 //GET USER
-router.get("/:id", async(req, res)=>{
-    try{
-        const user = await User.findById(req.params.id);
-        const {password, ...others} = user._doc
-        res.status(200).json(others);
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
